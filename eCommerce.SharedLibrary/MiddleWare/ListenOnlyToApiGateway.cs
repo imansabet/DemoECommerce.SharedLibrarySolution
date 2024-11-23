badc111a91c391343851fb934bd000d949be9878
iman <dev.imansabet@gmail.com>
2024-11-23 21:22:47 +0330

    Restrict access to API Gateway using custom middleware

---
+++ eCommerce.SharedLibrary/MiddleWare/ListenOnlyToApiGateway.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace eCommerce.SharedLibrary.MiddleWare;
+
+public class ListenOnlyToApiGateway(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context) 
+    {
+        // Extract Specific Header From Request
+        var signedHeader = context.Request.Headers["Api-gateway"];
+
+        // null ? Request not coming from Gateway
+        if (signedHeader.FirstOrDefault() is null)
+        {
+            context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+            await context.Response.WriteAsync("Sorry , Service Is Unavailable;");
+            return;
+        }
+        else 
+        {
+            await next(context);
+        }
+
+    } 
+}
