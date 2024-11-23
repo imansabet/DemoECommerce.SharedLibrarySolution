b6aa5257bed368fe1093de674cb610749b0e22c2
iman <dev.imansabet@gmail.com>
2024-11-23 19:35:47 +0330

    Add generic Interface for CRUD Operation Abstraction

---
+++ eCommerce.SharedLibrary/Interface/IGenericInterface.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Linq.Expressions;
+
+namespace eCommerce.SharedLibrary.Interface; 
+
+public interface IGenericInterface<T> where T : class
+{
+    Task<Response.Response> CreateAsync(T entity);  
+    Task<Response.Response> UpdateAsync(T entity);  
+    Task<Response.Response> DeleteAsync(T entity);  
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> FindByidAsync(int id);
+    Task<T> GetByAsync(Expression<Func<T,bool>> predicate);
+
+}
