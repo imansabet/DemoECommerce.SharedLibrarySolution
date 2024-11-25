ab6808b1c1695eb3afd120b69718633899096e3a
iman <dev.imansabet@gmail.com>
2024-11-25 23:02:41 +0330

    Products Controller : Add Get Method

---
+++ DemoECommerceProductApiSolution/ProductApi.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Application.DTOs;
+using ProductApi.Application.DTOs.Conversions;
+using ProductApi.Application.Interfaces;
+
+namespace ProductApi.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(IProduct productInterface) : ControllerBase
+    {
+        [HttpGet] 
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProducts() 
+        {
+            var products = await productInterface.GetAllAsync();
+            if (!products.Any())
+                return NotFound("No Products Detected in Db");
+
+            var (_, list) = ProductConversion.FromEntity(null!,products);
+            return list.Any() ? Ok(list) : NotFound("No Product Found");
+        }  
+    }
+}
